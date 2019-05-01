dc5006c312561211a1e75d4857c66941ee7285fd
Dominiquenw <domnwright18@gmail.com>
2019-05-01 18:35:49 -0400

    index.handlebars needs more work

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE burgers_db;
+
+INSERT INTO burgers (id, burger_name, devoured, date)
+
+VALUES 
+(null, "Cheese Burger", FALSE, null),
+(null, "Double Cheese Burger", FALSE, null),
+(null, "Bacon Cheese Burger", FALSE, null),
+(null, "Hamburger", FALSE, null),
+(null, "Double Bacon Cheese Burger", FALSE, null);
